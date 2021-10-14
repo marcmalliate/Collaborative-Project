4144c598371c3102f3f536dbdbfc88e5a2fa2626
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-14 21:52:31 +1030

    fetching APIs, currently working

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,10 @@
 console.log("test")
-
-
-
-
-
-
-
+var bodyEl = document.querySelector("body");
 
 
 function getApi() {
-    var ApiKey = "881d8641320234ecee7d0f7a4de6ff8d";
-    var requestUrl = 'https://developer.marvel.com/v1/public/characters';
+    ApiKey = "a626e5d44818f075594e06593d461dce";
+    var requestUrl = "https://gateway.marvel.com:443/v1/public/characters?apikey=a626e5d44818f075594e06593d461dce";
 
     fetch(requestUrl)
         .then(function (response) {
@@ -18,4 +12,33 @@ function getApi() {
         })
         .then(function (data) {
             console.log(data);
-        });
+        })
+};
+
+getApi();
+
+
+
+function getApi2() {
+    // need to make it that 
+    var superHero = "ironman"
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=iUE06hTiTNKEiqwUXpeFx0OlUfon6HSX&q=" + superHero + "&limit=25&offset=0&rating=g&lang=en";
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data[1].url);
+
+            for (i = 0; i<data.length; i++) {
+                var imgUrl = data[i].url;
+                var img = document.createElement('img');
+                img.setAttribute("src", )
+
+            }
+        })
+};
+
+getApi2();
