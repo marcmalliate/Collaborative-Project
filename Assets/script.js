65cd2c29e1a7853c2640272596f9dbb812577863
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-14 19:37:56 +1030

    fetch setup

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,21 @@
 console.log("test")
+
+
+
+
+
+
+
+
+
+function getApi() {
+    var ApiKey = "881d8641320234ecee7d0f7a4de6ff8d";
+    var requestUrl = 'https://developer.marvel.com/v1/public/characters';
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
