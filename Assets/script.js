3ca9a4b2894145919b411c8a8245f9f3dcba4a51
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-18 20:12:15 +1030

    giphy element and marvel photo element working

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 console.log("test")
 var bodyEl = document.querySelector("body");
 
-
 function getMarvelCharacter() {
     ApiKey = "a626e5d44818f075594e06593d461dce";
     var requestUrl = "https://gateway.marvel.com:443/v1/public/characters?apikey=a626e5d44818f075594e06593d461dce";
@@ -13,6 +12,12 @@ function getMarvelCharacter() {
         .then(function (data) {
             console.log(data);
             console.log(data.data.results[1].thumbnail.path + "." + data.data.results[1].thumbnail.extension)
+
+            var imgUrl = data.data.results[1].thumbnail.path + "." + data.data.results[1].thumbnail.extension;
+            var img = document.createElement('img');
+            img.setAttribute("src", imgUrl);
+            img.setAttribute("width", 250);
+            bodyEl.appendChild(img);
         })
 };
 
@@ -20,9 +25,12 @@ getMarvelCharacter();
 
 
 function getGiphy() {
-    // need to make it that superhero is populated via input query
-    var superHero = "ironman"
-    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=iUE06hTiTNKEiqwUXpeFx0OlUfon6HSX&q=" + superHero + "&limit=25&offset=0&rating=g&lang=en";
+    // superhero array to loop through
+    for (i = 0; i<20; i++) {
+    var superHeroArray = ["spiderman", "ironman", "thor", "loki", "captainamceria", "hulk", "thanos", "wolverine", "blackpanther", "natasharomanoff", "captainmarvel", "shuri", "groot", "gamora", "falcon", "blackwidow", "shangchi", "wandamaximoff", "mystique", "silversurfer"];
+    // var superHero = "ironman"
+    var apiKey = "x0rZHGLHnNG2XixxUlW3r3Og3I6WVEvG"
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + superHeroArray[i] + "&limit=20&offset=0&rating=g&lang=en";
 
     fetch(requestUrl)
         .then(function (response) {
@@ -30,15 +38,20 @@ function getGiphy() {
         })
         .then(function (data) {
             console.log(data);
-            console.log(data.data[1].url);
+            console.log(data.data[1].embed_url);
 
-            for (i = 0; i<data.length; i++) {
-                var imgUrl = data[i].url;
-                var img = document.createElement('img');
-                img.setAttribute("src", )
+            // for (i = 0; i<20; i++) {
+                // CAUTION, DONT RUN 400 TIMES, WILL DRAIN YOUR API KEY
+                var imgUrl = data.data[1].embed_url;
+                var img = document.createElement('iframe');
+                img.setAttribute("src", imgUrl);
+                img.setAttribute("alt", '"Powered By GIPHY"');
+                img.setAttribute("width", 250);
+                bodyEl.appendChild(img);
 
-            }
+            // }
         })
-};
+    
+}};
 
 getGiphy();
