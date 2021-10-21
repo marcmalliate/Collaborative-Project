1e46b1deb9769aebd56df2c778e6e84655e455c0
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-21 16:43:17 +1030

    cleaning up JS comments

--- Assets/script.js
+++ Assets/script.js
@@ -7,13 +7,6 @@ var cardContainer = document.getElementById("cardContainer");
 var giphyContainer = document.getElementById("giphyContainer");
 var navItem = document.getElementsByClassName("navbar-item");
 
-// for (i = 1; i<=20; i++) {
-//     var card = document.createElement("div");
-//     card.setAttribute("class", "giphyCard");
-//     card.setAttribute("id", i);
-//     giphyContainer.appendChild(card);
-// };
-
 function getMarvelCharacter() {
     var superHeroArray = ["Thor", "Loki", "Captain America", "Hulk", "Thanos", "Wolverine", "Black Panther", "Groot", "Gamora", "Falcon", "Black Widow", "Storm", "Shang-Chi", "Beast", "Scarlet Witch", "Mystique", "Hawkeye", "Silver Surfer", "Professor X", "Magneto"];
     for (i = 1; i < 21; i++) {
@@ -37,7 +30,6 @@ function getMarvelCharacter() {
                     card.setAttribute("id", data.data.results[0].name);
                     card.setAttribute("class", "marvelCard");
                     img.setAttribute("src", imgUrl);
-                    img.setAttribute("width", 250);
                     title.textContent = titleUrl;
                     cardContainer.appendChild(card);
                     card.appendChild(title);
@@ -92,48 +84,5 @@ cardContainer.addEventListener("click", function(event) {
     
 })
 
-navItem.addEventListener("click", function (event){
-    window
-})
-
-
-// function getGiphy() {
-
-//     var apiKey = "x0rZHGLHnNG2XixxUlW3r3Og3I6WVEvG"
-//     var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + superHero + "&limit=20&offset=0&rating=g&lang=en";
-
-    
-//         fetch(requestUrl)
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 for (i = 1; i <= 20; i++) {
-//                 console.log(data);
-//                 console.log(data.data[i].images.original);
-
-//                 var cardGif = document.createElement("div");
-//                 var gifUrl = data.data[i].images.original.url;
-//                 var gif = document.createElement('img');
-//                 cardGif.setAttribute("id", i);
-//                 cardGif.setAttribute("class", "giphyCard");
-//                 gif.setAttribute("src", gifUrl);
-//                 giphyContainer.appendChild(cardGif);
-//                 cardGif.appendChild(gif);
-//                 }
-//             })
-
-    
-// };
-
-// // Mobile Menu
-// const burgerIcon = document.querySelector('burger');
-// const navbarMenu = document.querySelector('#nav-links');
-
-// burgerIcon.addEventListener('click', () =>{
-//     navbarMenu.classList.toggle('is-active');
-// });
-
-// getGiphy();
 
 
